e0f406065df577f4c5bbed474d07d44f798ec1bd
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2023-04-03 12:59:14 +0200

    Changed project IDs to only have 5 decimals

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,6 +1,6 @@
 export default class Project {
   constructor(name) {
-    this.id = Math.random() * 100;
+    this.id = (Math.random() * 100).toFixed(5);
     this.name = name;
     this.todoList = [];
   }
