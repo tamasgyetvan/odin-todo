48689ba4c214435c6318574faa31fcb3cfadf8cb
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2023-04-08 17:24:53 +0200

    Remove duplicate event listener for deleteProjectButtons

--- src/index.js
+++ src/index.js
@@ -4,17 +4,10 @@ import { emptyInputPlaceholder } from "./modules/formValidation";
 let projectList = [];
 
 const createProjectBtn = document.getElementById("addProjectsButton");
-const deleteProjectButtons = document.querySelectorAll(".deleteProjectButton");
 
 createProjectBtn.addEventListener("click", function () {
   addNewProject();
   emptyInputPlaceholder();
-});
-
-deleteProjectButtons.forEach((deleteProjectbtn) => {
-  deleteProjectbtn.addEventListener("click", function () {
-    console.log("test");
-  });
 });
 
 export { projectList };
