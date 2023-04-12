21435303ea74241bc61e3bfcd54618b24dbb7714
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2023-04-12 20:48:46 +0200

    Added a function that sets object's active property true based on click event

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,7 +1,29 @@
+import { projectList } from "../index";
 export default class Project {
   constructor(name) {
     this.id = (Math.random() * 100).toFixed(5);
     this.name = name;
     this.todoList = [];
+    this.active = false;
   }
 }
+
+function setProjectStatusActive() {
+  const activeProjectObjectID = document
+    .querySelector(".active")
+    .getAttribute("data-id");
+
+  /*let activeObject = projectList.find((x) => (x.id = activeProjectObjectID)); 
+
+  activeObject.active = true;*/
+
+  projectList.forEach((project) => {
+    if (project.id === activeProjectObjectID) {
+      project.active = true;
+    } else {
+      project.active = false;
+    }
+  });
+}
+
+export { setProjectStatusActive };
